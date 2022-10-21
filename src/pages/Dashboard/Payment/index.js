4cd70b5f26cfa89d49b62eb3688db8ae43e10c66
Caio <caiosobreira77@gmail.com>
2022-10-20 21:16:09 -0300

    chore: inscription warnnig on payment screen

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -1,3 +1,49 @@
+import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import useEnrollment from '../../../hooks/api/useEnrollment';
+import Typography from '@material-ui/core/Typography';
+
+import EmptyMessage from './EmptyMessage';
 export default function Payment() {
-  return 'Pagamento: Em breve!';
+  const navigate = useNavigate();
+
+  const { enrollment } = useEnrollment();
+  const [isEnrolled, setisEnrolled] = useState(EmptyMessage);
+ 
+  useEffect(() => {
+    if (enrollment) {
+      setisEnrolled('');
+    }
+  }, [enrollment]);
+
+  function changePage() {
+    navigate('/dashboard/subscription');
+  }
+  return (
+    <>
+      <StyledTypography variant="h4">
+        Ingresso e pagamento
+      </StyledTypography>
+      <Container onClick={changePage}>
+        {isEnrolled === '' ? EmptyMessage : isEnrolled }
+      </Container>
+    </>
+    
+  );
 }
+
+const StyledTypography = styled(Typography)`
+margin-bottom: 20px!important;
+`;
+
+const Container = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+
+width: 100%;
+height: 70%;
+
+`;
+

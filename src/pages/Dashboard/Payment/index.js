21f5561fdc93583629e359f24ecd8584b7e3a509
tiagodlb <tigolimab223@gmail.com>
2022-10-20 23:27:45 -0300

    feature: ticket booking option

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -1,3 +1,25 @@
+import { useEffect, useState } from 'react';
+import useEnrollment from '../../../hooks/api/useEnrollment';
+import Typography from '@material-ui/core/Typography';
+import styled from 'styled-components';
+import TicketsForm from '../../../components/Payment/Options';
+
 export default function Payment() {
-  return 'Pagamento: Em breve!';
+  const { enrollment } = useEnrollment();
+  const [isEnrolled, setisEnrolled] = useState('Você precisa completar sua inscrição antes de prosseguir pra escolha de ingresso');
+  useEffect(() => {
+    if (enrollment) {
+      setisEnrolled('');
+    }
+  }, [enrollment]);
+  return (
+    <>
+      <StyledTypography variant="h4">Ingresso e Pagamentos</StyledTypography>
+      {isEnrolled === '' ? <TicketsForm /> : isEnrolled}
+    </> 
+  );
 }
+
+const StyledTypography = styled(Typography)`
+  margin-bottom: 20px!important;
+`;

5b5eb96b43a4fd0a781c9788d05f221e8039b38b
ViniciusTerra <viniterra33@gmail.com>
2022-10-22 01:56:32 -0300

    add PaymentSelection component

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -1,49 +1,31 @@
 import styled from 'styled-components';
-import { useNavigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import useEnrollment from '../../../hooks/api/useEnrollment';
 import Typography from '@material-ui/core/Typography';
-
 import EmptyMessage from './EmptyMessage';
-export default function Payment() {
-  const navigate = useNavigate();
+import PaymentSelection from '../../../components/PaymentSelection/index';
 
+export default function Payment() {
   const { enrollment } = useEnrollment();
-  const [isEnrolled, setisEnrolled] = useState(EmptyMessage);
- 
+  const [isEnrolled, setisEnrolled] = useState(false);
+
   useEffect(() => {
     if (enrollment) {
-      setisEnrolled('');
+      setisEnrolled(true);
+    }
+    else{
+      setisEnrolled(false);
     }
   }, [enrollment]);
 
-  function changePage() {
-    navigate('/dashboard/subscription');
-  }
   return (
     <>
-      <StyledTypography variant="h4">
-        Ingresso e pagamento
-      </StyledTypography>
-      <Container onClick={changePage}>
-        {isEnrolled === '' ? EmptyMessage : isEnrolled }
-      </Container>
+      <StyledTypography variant="h4">Ingresso e pagamento</StyledTypography>
+      {isEnrolled === true ? PaymentSelection() : EmptyMessage()}
     </>
-    
   );
 }
 
 const StyledTypography = styled(Typography)`
-margin-bottom: 20px!important;
+  margin-bottom: 20px !important;
 `;
-
-const Container = styled.div`
-display: flex;
-justify-content: center;
-align-items: center;
-
-width: 100%;
-height: 70%;
-
-`;
-
